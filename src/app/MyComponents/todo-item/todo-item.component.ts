156a4767e7e038d035c4098978d8f611fde7a19d
Zaid <zaidsayed910@gmail.com>
2021-12-12 22:00:09 +0530

    first commit

---
+++ src/app/MyComponents/todo-item/todo-item.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Todo } from 'src/app/Todo';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+export class TodoItemComponent implements OnInit {
+    @Input() todo : Todo;
+    @Input() i : number;
+    @Output() todoDelete: EventEmitter<Todo> = new EventEmitter();
+    @Output() todoCheckbox: EventEmitter<Todo> = new EventEmitter();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onClick(todo:Todo){
+   this.todoDelete.emit(todo);
+    console.log("click function has triggred")
+  }
+  onCheckboxClick(todo){
+    this.todoCheckbox.emit(todo);
+  }
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './MyComponents/about/about.component';
+import { TodosComponent } from './MyComponents/todos/todos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: '', component: TodosComponent },
+    { path: 'about', component: AboutComponent },
+    // { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
